192d18cf5fb7789f6200ad3afef5b4fc93abdc51
Aslam-wokle <aslamhamdi.ah08@gmail.com>
2020-11-09 14:03:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello guys!, I am Aslam, the Cloud Computer expert!. Please go to my github account https://github.com/Aslam-wokle/HelloworldPHP.git";
+echo "Hello guys!, I am Aiman, the Cloud Computer expert!. Please go to my github account https://github.com/Aslam-wokle/HelloworldPHP.git";
 ?>
 
 </body>
