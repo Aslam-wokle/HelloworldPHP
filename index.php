2abee08b3de1b677839b862983be45efcdc24e41
Aslam-wokle <aslamhamdi.ah08@gmail.com>
2020-11-09 11:45:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello guys!, I am Aslam, the Cloud Computer expert!. Please go to my github account ";
+?>
+
+</body>
+</html>
